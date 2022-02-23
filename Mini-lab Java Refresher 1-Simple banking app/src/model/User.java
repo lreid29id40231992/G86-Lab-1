c991f46f62640a07d66b6b6717e3fe4cd0440990
40206129 <jkeenan189@qub.ac.uk>
2022-02-23 22:17:26 +0000

    Camel case changes

--- Mini-lab Java Refresher 1-Simple banking app/src/model/User.java
+++ Mini-lab Java Refresher 1-Simple banking app/src/model/User.java
@@ -4,22 +4,22 @@ public class User {
 
 	private String username; //email address
 	private String password;
-	private String first_name;
-	private String last_name;
-	private String mobile_number;
+	private String firstName;
+	private String lastName;
+	private String mobileNumber;
 	
-	public User(String username, String password, String first_name, String last_name, String mobile_number) {
+	public User(String username, String password, String firstName, String lastName, String mobileNumber) {
 		super();
 		this.username = username;
 		this.password = password;
-		this.first_name = first_name;
-		this.last_name = last_name;
-		this.mobile_number = mobile_number;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.mobileNumber = mobileNumber;
 	}
 	
 	public String toString() {
 		
-		return username + ", " + password + ", " + first_name + ", " + last_name + ", " + mobile_number;
+		return username + ", " + password + ", " + firstName + ", " + lastName + ", " + mobileNumber;
 		
 	}
 	
@@ -36,23 +36,23 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getFirst_name() {
-		return first_name;
+	public String getFirstName() {
+		return firstName;
 	}
-	public void setFirst_name(String first_name) {
-		this.first_name = first_name;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-	public String getLast_name() {
-		return last_name;
+	public String getLastName() {
+		return lastName;
 	}
-	public void setLast_name(String last_name) {
-		this.last_name = last_name;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-	public String getMobile_number() {
-		return mobile_number;
+	public String getMobileNumber() {
+		return mobileNumber;
 	}
-	public void setMobile_number(String mobile_number) {
-		this.mobile_number = mobile_number;
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
 	}
 	
 }
